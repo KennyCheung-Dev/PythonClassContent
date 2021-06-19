0eb6b357f1dc28cafc3e4fd9edda120348ce214a
Kenny <kennycheung388@gmail.com>
2021-06-18 23:14:10 -0700

    Crawler Lecture1

--- PythonAlgo/PythonDS/temp.py
+++ PythonAlgo/PythonDS/temp.py
@@ -164,6 +164,36 @@ Output: False
 
 
 
+'''
+Beibao
+
+There is a bag you can fill in with items
+Items can be cut into different portions freely
+Each item has the its amount worth, and the weight
+
+First line contains m n   (e.g. 150 7) where m represents the weight that would fit
+The next n lines will represents (n-1)th item's amount worth and weight
+
+Please find out the max amount worth that can fit in the bag
+
+Input:
+150 7
+10 35
+40 30
+30 60
+50 50
+35 40
+40 10
+30 25
+
+Output:
+190.625
+
+'''
+
+
+
+
 
 
 
